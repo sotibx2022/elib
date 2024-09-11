c2443f6e4e3e526b4c8881d22ba029ffffc9c577
Binaya <sbinayaraj@gmail.com>
2024-09-11 15:32:59 +0300

    Dashboard URL set to the cors middleware

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,19 @@ import userRouter from "./users/userRouter";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import bookRouter  from "./books/bookRouter";
 import { config } from "./config/configuration";
+const allowedOrigins = [config.FRONTEND_URL, config.DASHBOARD_URL];
 const app = express();
 app.use(express.json()); // This line is crucial
 app.use(cors({
-    origin:config.FRONTEND_URL
-}))
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS')); // Reject the request
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow methods as needed
+}));
 app.use('/api/users/',userRouter)
 app.use('/api/books', bookRouter);
 app.use(globalErrorHandler);
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,6 +5,7 @@ PORT:process.env.PORT,
 CONNECTION_STRING:process.env.CONNECTION_STRING,
 ENV:process.env.ENV,
 SECRET_KEY:process.env.SECRET_KEY,
-FRONTEND_URL:process.env.FRONTEND_URL
+FRONTEND_URL:process.env.FRONTEND_URL,
+DASHBOARD_URL:process.env.DASHBOARD_URL
 };
 export const config = Object.freeze(_config);
